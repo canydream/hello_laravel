5b227c3c7e788967252e60d750f6c2620e4b55a5
canydream <canydream@qq.com>
2020-02-17 11:47:52 +0000

    基础页面

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <!-- 第一个参数是该区块的变量名称，第二个参数是默认值 -->
+    <title>@yield('title', 'Weibo App') - Laravel 新手入门教程</title>
+  </head>
+  <body>
+    @yield('content')
+  </body>
+</html>
