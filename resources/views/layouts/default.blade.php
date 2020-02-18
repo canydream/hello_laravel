14ad470e39963a7dc01ea82c473f09d32b02cfd5
canydream <canydream@qq.com>
2020-02-18 01:54:16 +0000

    静态文件浏览器缓存问题

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -3,6 +3,7 @@
   <head>
     <title>@yield('title', 'Weibo App') - Laravel 入门教程</title>
     <!-- <link rel="stylesheet" href="/css/app.css"> -->
+    <!-- 静态文件的动态化 -->
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
   </head>
   <body>
