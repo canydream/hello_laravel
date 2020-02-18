a8defc5b715866af71cdf0a1923ec5b5c9d00083
canydream <canydream@qq.com>
2020-02-18 05:11:58 +0000

    用户注册页面

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,6 @@ Route::get('/', 'StaticPagesController@home')->name('home');
 Route::get('/help', 'StaticPagesController@help')->name('help');
 Route::get('/about', 'StaticPagesController@about')->name('about');
 
+Route::get('signup', 'UsersController@create')->name('signup');
+
+//注册路由时，URI signup 和 /signup 从使用上来看，并无区别，Laravel 框架兼容这两种写法。
