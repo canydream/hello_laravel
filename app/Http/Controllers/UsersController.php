135b77779ee5abcbbf34dfa5e3252a0ebdd4dafb
canydream <canydream@qq.com>
2020-02-24 08:20:04 +0000

    用户显示页面

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;//need
+
 
 class UsersController extends Controller
 {
@@ -10,5 +12,9 @@ class UsersController extends Controller
     {
         return view('users.create');
     }
+    public function show(User $user)
+    {
+        return view('users.show', compact('user'));
+    }
 }
-
+// 我们将用户对象 $user 通过 compact 方法转化为一个关联数组，并作为第二个参数传递给 view 方法，将数据与视图进行绑定。
--- routes/web.php
+++ routes/web.php
@@ -20,5 +20,7 @@ Route::get('/help', 'StaticPagesController@help')->name('help');
 Route::get('/about', 'StaticPagesController@about')->name('about');
 
 Route::get('signup', 'UsersController@create')->name('signup');
+Route::resource('users', 'UsersController');
 
 //注册路由时，URI signup 和 /signup 从使用上来看，并无区别，Laravel 框架兼容这两种写法。
+
